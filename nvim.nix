0daa7cd556080a5879ce2839798758541fc236ed
Martin <66840021+Mcrtin@users.noreply.github.com>
2024-12-13 16:40:44 +0100

    chore: update dependencies

--- nvim.nix
+++ nvim.nix
@@ -72,7 +72,7 @@
     lastplace.enable = true;
     hex.enable = true;
     telescope.enable = true;
-    nvim-colorizer.enable = true; # color colorcodes
+    colorizer.enable = true; # color colorcodes
     nvim-autopairs.enable = true;
     web-devicons.enable = true;
 
@@ -138,14 +138,16 @@
 
     noice = {
       enable = true;
-      notify.enabled = true;
-      lsp = {
-        override = {
-          "vim.lsp.util.convert_input_to_markdown_lines" = true;
-          "vim.lsp.util.stylize_markdown" = true;
-          "cmp.entry.get_documentation" = true;
+      settings = {
+        notify.enabled = true;
+        lsp = {
+          override = {
+            "vim.lsp.util.convert_input_to_markdown_lines" = true;
+            "vim.lsp.util.stylize_markdown" = true;
+            "cmp.entry.get_documentation" = true;
+          };
+          progress.enabled = false;
         };
-        progress.enabled = false;
       };
     };
 
