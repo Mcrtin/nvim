da885eb5232aedf20a118d1a22f543c27654ffb2
Martin <66840021+Mcrtin@users.noreply.github.com>
2025-10-04 18:40:09 +0200

    remove tab control of the bufferline for ctrl-i to work

--- bufferline.nix
+++ bufferline.nix
@@ -7,20 +7,6 @@
   keymaps = [
     {
       mode = "n";
-      key = "<Tab>";
-      action = "<cmd>BufferLineCycleNext<cr>";
-      options.desc = "Cycle to next buffer";
-    }
-
-    {
-      mode = "n";
-      key = "<S-Tab>";
-      action = "<cmd>BufferLineCyclePrev<cr>";
-      options.desc = "Cycle to previous buffer";
-    }
-
-    {
-      mode = "n";
       key = "<S-l>";
       action = "<cmd>BufferLineCycleNext<cr>";
       options.desc = "Cycle to next buffer";
